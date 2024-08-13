0137846fe81c4b90995244a6ae5b1f6e84eace50
Nischal876 <nischalaryal11@gmail.com>
2024-08-13 11:46:43 +0545

    Refactor: filterChain method of SecurityConfig - All HTTP requests to cashcards/ endpoints are required to be authenticated using HTTP Basic Authentication security (username and password).

--- src/main/java/com/example/cashcard/SecurityConfig.java
+++ src/main/java/com/example/cashcard/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.cashcard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,13 @@ class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                .requestMatchers("/cashcards/**")
+                .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf-> csrf.disable());
+
+
         return http.build();
     }
 
